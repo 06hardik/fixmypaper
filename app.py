8baafccb1149631271edc32738cdda1c92f27bf6
Aayushi Jain <iamaayushirocks@gmail.com>
2026-03-25 20:36:06 +0530

    seperated frontend and backend

--- app.py
+++ app.py
@@ -267,4 +267,4 @@ def health():
 if __name__ == '__main__':
     print("Starting Research Paper Error Checker…")
     print("Open your browser to: http://localhost:5001")
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run( host='0.0.0.0', port=5001)
